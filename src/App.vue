3d2eda2130aabbf2760bfae38be7bc6ddb75cb89
Vinicius de Oliveira <ti.vinicius.dev@gmail.com>
2021-11-26 16:33:07 -0300

    Primeiros estudos em Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,129 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <h1>Hello Vuex</h1>
+
+    <input type="text" v-model="name" placeholder="name...">
+    <input type="text" v-model="message" placeholder="message...">
+
+    <!-- EVENTO PARA ENVIAR ATIVAR MÉTODO QUE MANDA PRA STORE -->
+    <button @click.prevent.stop="save(), sayMessage()">Salvar</button>
+
+    <!-- PEGANDO DIRETO DA STORE STATE -->
+    <h3>Vindo do store state: </h3>
+    <p>Name: {{ $store.state.user.name }}</p>
+    <p>Message: {{ $store.state.user.message }}</p>
+    <br><br><br>
+
+    <!-- PEGANDO GET DA STORE GETTER -->
+    <h3>Vindo do store getter: </h3>
+    <p>Name: {{ $store.getters.name }}</p>
+    <p>Message: {{ $store.getters.message }}</p>
+
+    <h4>Com mapGetter: </h4>
+    <p>Name: {{ anotherName }}</p>
+    <p>Message: {{ helloMsg }}</p>
+    
+    <br><br><br>
+
+    <h3>Mapeamento da state</h3>
+    <pre>
+      {{ user }}
+    </pre>
+
+    <br><br><br>
+    <!-- PEGANDO GET DA COMPUTED -->
+    <h3>Vindo do get da computed: </h3>
+    <input type="text" v-model="email">
+    <h3>{{ email }}</h3>
+    <br><br><br>
+
+    <h3>Exemplo carrinho de compras</h3>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        <p>{{ product.name }}</p>
+        <!-- EVENTO PARA ENVIAR ATIVAR MÉTODO QUE MANDA PRA STORE -->
+        <button @click="addToCart(product)">Add to cart</button>
+        <button @click="removeFromCart(product)">Remover</button>
+      </li>
+    </ul>
+
+  <pre>
+    {{ $store.state.cart }}
+  </pre>
+
+  <br><br><br>
+  <h2>Acessando módulos</h2>
+  <p>{{ $store.state.colors }}</p>
+
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { mapState, mapGetters, mapMutations } from 'vuex'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: 'Produto 1'
+        },
+        {
+          id: 2,
+          name: 'Produto 2'
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapState({
+      user: state => {
+        return state.user
+      }
+    }),
+
+    ...mapGetters(
+      {
+        anotherName: 'name',
+        helloMsg: 'message'
+      }
+    ),
+    email: {
+      get() {
+        // return this.$store.state.user.email
+        return this.user.email
+      },
+      set(value) {
+        this.$store.commit('saveEmail', value)
+      }
+    }
+  },
+  methods: {
+    ...mapMutations(['ADD_PRODUCT_TO_CART', 'REMOVE_FROM_CART']),
+    //MANDANDO PRA STORE COM EVENTOS E MÉTODOS
+    addToCart(product) {
+      // this.$store.commit('ADD_PRODUCT_TO_CART', product)
+      this.ADD_PRODUCT_TO_CART(product)
+    },
+    removeFromCart(product) {
+      this.REMOVE_FROM_CART(product)
+    },
+    save() {
+      //action
+      this.$store.dispatch('saveName', this.name)
+      
+      //mutation
+    },
+    sayMessage() {
+      this.$store.commit('sayMessage', this.message)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,68 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  strict: true,
   state: {
+    user: {
+      name: '',
+      message: '',
+      email: 'email padrao'
+    },
+    cart: []
   },
   mutations: {
+    //CART  
+
+    ADD_PRODUCT_TO_CART(state, payload) {
+      const existingProduct = state.cart.find(o => o.id === payload.id)
+      if(existingProduct) {
+        existingProduct.qty += 1
+      } else {
+        payload.qty = 1
+        state.cart.push(payload)
+      }
+    },
+    REMOVE_FROM_CART(state, payload) {
+      const existingProduct = state.cart.find(o => o.id === payload.id)
+      if(existingProduct && existingProduct.qty > 1) {
+        existingProduct.qty -= 1} else {
+          const idx = state.cart.findIndex(o => o.id === payload.id)
+          state.cart.splice(idx, 1)
+        }
+    },
+
+    //USER
+    
+    saveName(state, payload) {
+      state.user.name = payload
+    },
+    sayMessage(state, payload) {
+      state.user.message = payload
+    },
+    saveEmail(state, email) {
+      state.user.email = email
+    }
   },
   actions: {
+    saveName(context, payload) {
+      context.commit('saveName', payload)
+    }
+  },
+  getters: {
+    name(state) {
+      return state.user.name
+    }, 
+    message(state) {
+      return state.user.message
+    }
   },
   modules: {
+    colors: {
+      state: () => ({
+        color: 'Blue'
+      }),
+      mutations: {},
+      //ABSTRAIR CAMADAS...
+    }
   }
 })
